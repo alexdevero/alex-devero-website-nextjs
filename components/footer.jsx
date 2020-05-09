913f9c7a8289fc054398ca20fb56a427f858f382
Alex Devero <deveroalex@gmail.com>
2020-05-09 11:05:46 +0200

    Add footer component

---
+++ components/footer.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className="footer">
+    <nav>
+      <ul className="footer__media">
+        <li>
+          <a href="https://twitter.com/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-twitter" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.facebook.com/deveroalex" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-facebook" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://instagram.com/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-instagram" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://cz.linkedin.com/in/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-linkedin" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://dribbble.com/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-dribbble" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://github.com/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-github" />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://codepen.io/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-codepen" />
+          </a>
+        </li>
+
+        {/*<li>
+          <a href="https://angel.co/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-angellist" />
+          </a>
+        </li>*/}
+
+        {/* <li>
+          <a href="https://stackoverflow.com/story/alexdevero" rel="noopener noreferrer" target="_blank">
+            <span className="fab fa-stack-overflow" />
+          </a>
+        </li> */}
+      </ul>
+    </nav>
+
+    <small className="footer__copy">
+      {/* Copyright &copy; <time dateTime={new Date()}>{new Date().getFullYear()}</time> Alex Devero */}
+    </small>
+  </footer>
+)
+
+export default Footer
