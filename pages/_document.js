84fad32ebbdd1fa2246ae29ccf4b5f1566243f6f
Alex Devero <deveroalex@gmail.com>
2020-05-09 11:05:56 +0200

    Fix html in document

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,8 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <html lang="en" />
-
           {/*<!-- Typeface Raleway -->*/}
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900&amp;subset=latin-ext" />
 
