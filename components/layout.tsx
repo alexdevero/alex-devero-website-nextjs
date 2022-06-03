eff7e045fdb5ea6d502ec82a4a60af2c30f2dc68
Alex Devero <deveroalex@gmail.com>
2022-06-03 09:32:39 +0200

    Fix layout children type

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import Footer from './footer'
 import Header from './header'
 
-export const Layout = memo(({ children, page, title }: { children: ReactChild, page: string, title: string}) => (
+export const Layout = memo(({ children, page, title }: { children: ReactChild | ReactChild[], page: string, title: string}) => (
   <>
     {title && <Head>
       <title>{title}</title>
