9fc554a671b6cf77ee26c3c135608dadbae2b37f
Alex Devero <deveroalex@gmail.com>
2022-06-03 09:01:50 +0200

    Upgrade deps

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// const isProd = process.env.NODE_ENV === 'production'
+// const assetPrefix = isProd ? 'https://alexdevero.com/demos/ad-next/' : ''
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  // assetPrefix: isProd ? 'https://alexdevero.com/demos/ad-next/' : '',
+  // env: {
+  //   ASSET_PREFIX: assetPrefix,
+  // }
+}
