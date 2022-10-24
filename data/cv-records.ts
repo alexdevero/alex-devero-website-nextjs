95971723e0491d195ed8d714e40683c81efb1633
Alex Devero <deveroalex@gmail.com>
2022-10-24 15:43:19 +0200

    Add cv data

---
+++ data/cv-records.ts
@@ -0,0 +1,66 @@
+export const cvRecords = {
+  personalInfo: {
+    name: 'Alex Devero',
+    age: 31,
+    address: 'Prague, Czech Republic',
+    phone: '',
+    email: '',
+    website: 'https://alexdevero.com',
+    github: 'https://github.com/alexdevero',
+    linkedin: 'https://cz.linkedin.com/in/alexdevero',
+  },
+  education: [
+    {
+      school: '',
+      degree: '-',
+      major: '',
+      date: 'yyyy-yyyy'
+    }
+  ],
+  experience: [
+    {
+      company: 'Company A',
+      title: 'Software Engineer',
+      date: '2014-2016',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl. Donec euismod, nisl vel aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet nisl.'
+    }
+  ],
+  skills: [
+    {
+      name: 'HTML',
+      level: 90
+    },
+    {
+      name: 'CSS',
+      level: 85
+    },
+    {
+      name: 'JavaScript',
+      level: 80
+    },
+    {
+      name: 'TypeScript',
+      level: 75
+    },
+    {
+      name: 'React',
+      level: 85
+    },
+    {
+      name: 'Node',
+      level: 50
+    },
+    {
+      name: 'MongoDB',
+      level: 10
+    },
+    {
+      name: 'PostgreSQL',
+      level: 10
+    },
+    {
+      name: 'Git',
+      level: 75
+    }
+  ]
+}
