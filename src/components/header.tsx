a6c2ed587332ab6c575dddf5a99406ba123aaeed
Alex Devero <deveroalex@gmail.com>
2023-11-18 17:32:48 +0100

    Underline active navigation link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,48 +1,66 @@
 'use client'
 
 import { useState } from 'react'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
+  const pathname = usePathname()
   const [isMobileMenuVisible, toggleMobileMenu] = useState(false)
 
   return (
-    <header className="header">
-      <nav className="flex py-6">
-        <ul className="flex flex-1 items-center justify-center gap-4">
+    <header className='header'>
+      <nav className='flex py-6'>
+        <ul className='flex flex-1 items-center justify-center gap-4'>
           <li>
-            <button
-              className="nav__toggler nav__toggler--close"
-              onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
-            >
+            <button className='nav__toggler nav__toggler--close' onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
               <span />
               <span />
             </button>
           </li>
 
           <li>
-            <span onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
-              <Link href="/">Home</Link>
+            <span
+              className={pathname === '/' ? 'underline' : ''}
+              onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
+            >
+              <Link href='/'>Home</Link>
             </span>
           </li>
 
           <li>
-            <span onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
-              <Link href="/projects">Projects</Link>
+            <span
+              className={pathname === '/projects' ? 'underline' : ''}
+              onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
+            >
+              <Link href='/projects'>Projects</Link>
             </span>
           </li>
 
           <li>
-            <span onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
-              <Link href="/about">About</Link>
+            <span
+              className={pathname === '/about' ? 'underline' : ''}
+              onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
+            >
+              <Link href='/about'>About</Link>
+            </span>
+          </li>
+
+          <li>
+            <span
+              className={pathname === '/cv' ? 'underline' : ''}
+              onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
+            >
+              <Link href='/cv'>CV</Link>
             </span>
           </li>
 
           <li>
             <a
-              href="https://blog.alexdevero.com/"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='https://blog.alexdevero.com/'
+              rel='noopener noreferrer'
+              target='_blank'
               onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
             >
               Blog
@@ -51,9 +69,9 @@ const Header = () => {
 
           <li>
             <a
-              href="https://devero.io/"
-              rel="noopener noreferrer"
-              target="_blank"
+              href='https://devero.io/'
+              rel='noopener noreferrer'
+              target='_blank'
               onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
             >
               Devero
@@ -65,16 +83,16 @@ const Header = () => {
           </li> */}
 
           <li>
-            <span onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
-              <Link href="/contact">Contact</Link>
+            <span
+              className={pathname === '/contact' ? 'underline' : ''}
+              onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
+            >
+              <Link href='/contact'>Contact</Link>
             </span>
           </li>
         </ul>
 
-        <button
-          className="max-md:flex"
-          onClick={() => toggleMobileMenu(!isMobileMenuVisible)}
-        >
+        <button className='max-md:flex' onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
           <span />
           <span />
           <span />
