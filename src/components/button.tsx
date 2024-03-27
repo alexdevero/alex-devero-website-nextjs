e1fb378a0c83eaf885ed4fd5666deebaf2c7ae32
Alex Devero <deveroalex@gmail.com>
2024-03-27 10:23:54 +0100

    feat: add form for login page (not working yet)

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,15 +4,17 @@ import classNames from 'classnames'
 
 type Props = HTMLAttributes<HTMLButtonElement> & {
   children: string
+  disabled?: boolean
   type?: 'button' | 'submit' | 'reset'
   centered?: boolean
 }
 
-export const Button = ({ children, className, centered = true, type = 'button', ...rest }: Props) => (
+export const Button = ({ children, disabled, className, centered = true, type = 'button', ...rest }: Props) => (
   <button
     type={type}
     className={classNames('flex h-9 items-center justify-center rounded-md bg-blue-500 px-4 text-white', className, {
       'mx-auto': centered,
+      'bg-gray-400': disabled,
     })}
     {...rest}
   >
---
+++ src/components/pages/login-page.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '../button'
+import { Input } from '../form-elements/input'
+
+const formSchema = z.object({
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+  password: z.string().min(1, 'Password is required'),
+})
+
+const initialValues = {
+  email: '',
+  password: '',
+}
+
+type FormValues = typeof initialValues
+
+export const LoginPage = () => {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>({
+    defaultValues: initialValues,
+    resolver: zodResolver(formSchema),
+  })
+
+  const handleLogin = async (values: FormValues) => {
+    console.log('Login', values)
+  }
+
+  return (
+    <div>
+      <h1 className='mb-8 mt-8 text-center text-5xl font-bold'>Login</h1>
+
+      <div className='m-auto max-w-[280px]'>
+        <form className='flex flex-col gap-2' action='' onSubmit={handleSubmit(handleLogin)}>
+          <Input
+            id='email'
+            type='email'
+            label='E-mail:'
+            hasError={!!errors.email}
+            errorMessage={errors.email?.message}
+            {...register('email')}
+          />
+          <Input
+            id='password'
+            type='password'
+            label='Password:'
+            hasError={!!errors.password}
+            errorMessage={errors.password?.message}
+            {...register('password')}
+          />
+
+          <div>
+            <Button type='submit'>Login</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
