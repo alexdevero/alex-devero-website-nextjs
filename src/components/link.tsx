f7a7017ea07c9b3622d7a7d402fe4c78ead33faf
Alex Devero <deveroalex@gmail.com>
2024-12-25 11:01:07 +0100

    fix: add support for mailto for Link

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -3,6 +3,8 @@ import type { FC, PropsWithChildren } from 'react'
 import type { LinkProps } from 'next/link'
 import NextLink from 'next/link'
 
+import classNames from 'classnames'
+
 const isExternal = (href: LinkProps['href']) => href.toString().startsWith('http')
 
 type Props = {
@@ -11,10 +13,18 @@ type Props = {
 } & LinkProps
 
 export const Link: FC<PropsWithChildren<Props>> = ({ className, href, children, ...props }) => {
+  if (href.toString().startsWith('mailto:') || href.toString().startsWith('tel:')) {
+    return (
+      <a href={href.toString()} className={classNames(className, 'text-gray-500 underline')} {...props}>
+        {children}
+      </a>
+    )
+  }
+
   return (
     <NextLink
       href={href}
-      className={className}
+      className={classNames(className, 'text-gray-500 underline')}
       rel={isExternal(href) ? 'noopener noreferrer' : undefined}
       target={isExternal(href) ? '_blank' : undefined}
       {...props}
