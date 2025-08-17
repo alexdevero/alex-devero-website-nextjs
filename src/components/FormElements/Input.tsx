9208d3605777ac72504922ecfc2fc6f6bd416940
Alex Devero <deveroalex@gmail.com>
2025-08-17 19:21:40 +0200

    fix: update input styles

--- src/components/FormElements/Input.tsx
+++ src/components/FormElements/Input.tsx
@@ -1,14 +1,13 @@
+import classNames from 'classnames'
 import type { HTMLAttributes } from 'react'
 import { forwardRef } from 'react'
-
-import classNames from 'classnames'
 
 import { ErrorMessage } from './ErrorMessage'
 import { InputWrapper } from './InputWrapper'
 import { Label } from './Label'
 
 export const inputClasses =
-  'rounded-sm border border-gray-300 py-1 px-1.5 text-sm transition-all duration-300 hover:border-blue-400 hover:shadow-[0_0_0_2px_rgba(96,165,250,0.6)] dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300'
+  'rounded-[4px] border border-gray-200 py-1 px-1.5 text-sm transition-all duration-300 hover:border-blue-400 hover:shadow-[0_0_0_2px_rgba(96,165,250,0.6)] bg-gray-100 dark:border-gray-700 dark:bg-black-100 dark:text-white'
 export const inputErrorClasses = '!border-red-500 hover:!shadow-[0_0_0_2px_rgba(239,68,68,0.4)]'
 
 type Props = HTMLAttributes<HTMLInputElement> & {
